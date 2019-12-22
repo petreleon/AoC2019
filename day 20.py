d32afa8e85467ff6a51a9a3a1b6559a12eb3a97c
petreleon <petreleonardos@gmail.com>
2019-12-22 05:15:16 +0200

    Create day 20.py

---
+++ day 20.py
@@ -0,0 +1,104 @@
+import requests
+import pyperclip
+import itertools
+import functools
+import math
+import random
+from fractions import Fraction
+from intcode import *
+import sys
+from itertools import permutations
+
+from parse import *
+from copy import *
+
+import math
+
+jar = requests.cookies.RequestsCookieJar()
+
+jar.set('session', '53616c7465645f5f5bbe75d49d6fcfcac1e8502413e0a4e632529212b3de5ee3b6577f4ba97c30b41139d519f0583cfc',
+        domain='adventofcode.com')
+
+s = requests.Session()
+s.cookies = jar
+r = s.get('https://adventofcode.com/2019/day/21/input')
+response = 0
+
+input_text = r.text
+
+lines = input_text.splitlines()
+
+list_ = list(map(int, lines[0].split(',')))
+list_.extend([0]*10000)
+
+stringToInput = ''
+
+'''
+FIRST INPUT:
+
+OR A T
+AND B T
+AND C T
+NOT D J
+OR J T
+NOT T J
+WALK
+'''
+'''
+// not working
+SECOND INPUT:
+
+FIRST ATTEMPT
+OR A T
+AND B T
+AND C T
+NOT D J
+OR J T
+NOT T J
+OR D T
+AND I T
+OR H T
+AND T J
+NOT A T
+OR T J
+RUN
+
+SECOND ATTEMPT
+OR A T
+AND B T
+AND C T
+NOT T J
+AND D J
+AND E J
+AND F J
+RUN
+
+THIRD ATTEMPT
+
+OR A J
+AND E J
+
+'''
+
+indexOfInput = 0
+
+def input_():
+    global stringToInput
+    global indexOfInput
+    indexOfInput += 1
+    if indexOfInput >= len(stringToInput):
+        stringToInput = input()+'\n'
+        indexOfInput = 0
+
+    return ord(stringToInput[indexOfInput])
+
+
+def print_(val):
+    global response
+    if val > 256:
+        print(val)
+    else:
+        print(chr(val), end='')
+
+
+machine(input_, print_, list_)
